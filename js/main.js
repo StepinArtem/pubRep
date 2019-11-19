2da8c91e0732560b9a3a69d38ae2a1cbda8f2175
Artem <art93st93@mail.ru>
2019-11-19 20:05:15 +0300

    New commit

--- js/main.js
+++ js/main.js
@@ -16,7 +16,11 @@ require.config({
         testComponent:'components/testComponent', // тестовый компонет	
         mapChart:'components/mapChart',
         plotChart:'components/plotChart',
-        tableNew:'components/tableNew'
+        tableNew:'components/tableNew',
+        xmlToJson:'xml2json',
+        chart:'Chart.min',
+        utils:'utils',
+        axios:'axios.min'
         //--------------------------------------------------------
     },
     shim: { // разрешение зависимостей между модулями в [] указаны библиотеки и модули необходимые для работы
@@ -42,9 +46,9 @@ require.config({
 });
 //------------------------------------------------------------------------------------
 //ВНИМАНИЕ колличество аргументов в define должно соотвествовать, количеству аргументов в function 
-define(['jquery', 'bootstrapBundle', 'Vue','MainCatalog'],
+define(['jquery', 'bootstrapBundle', 'Vue','MainCatalog','xmlToJson','chart','utils','axios'],
 
-    function ($, bootstrapBundle, Vue,MainCatalog) {
+    function ($, bootstrapBundle, Vue,MainCatalog,xmlToJson,chart,utils,axios) {
         //-------------------------------------------------------------------------------------------------
         // инициализация 
         catalog = require('MainCatalog'); 
